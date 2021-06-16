4e72f782106d96fc562253d94d64ae167692aeb5
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-06-16 10:38:58 +0300

    Добавляет начальные файлы приложения

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+
+import './App.scss';
+
+class App extends Component {
+  // state = {
+
+  // };
+
+  // componentDidMount() {
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Image finder</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
